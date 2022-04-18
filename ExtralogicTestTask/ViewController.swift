b7cdee15fc55e71cd8aca3eef33d901dcffbcaa1
Andreev Ilya <AndreevIV.dev@gmail.com>
2022-04-18 14:37:40 +0300

    Initial Commit

---
+++ ExtralogicTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExtralogicTestTask
+//
+//  Created by Илья Андреев on 18.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
